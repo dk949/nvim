daa7e4d19953c6fd05e35b864841f843cc60347b
dk949 <56653556+dk949@users.noreply.github.com>
2022-11-20 19:11:46 +0000

    local fn

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,9 +1,10 @@
 require('misc.utils')
+local fn = vim.fn
 
-if not vim.fn.isdirectory(vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim') then
+if not fn.isdirectory(fn.stdpath('data')..'/site/pack/packer/start/packer.nvim') then
     warnPrint("WARNING: packer.nvim not found. Trying to download...")
-    vim.fn.system("git clone --depth 1 https://github.com/wbthomason/packer.nvim " .. vim.fn.stdpath('data') .."/site/pack/packer/start/packer.nvim")
-    if vim.v.shell_error ~= 0 then errPrint("ERROR: packer.nvim could not be downloaded") end
+    fn.system("git clone --depth 1 https://github.com/wbthomason/packer.nvim " .. fn.stdpath('data') .."/site/pack/packer/start/packer.nvim")
+    if v.shell_error ~= 0 then errPrint("ERROR: packer.nvim could not be downloaded") end
 end
 
 return require("packer").startup(function()
