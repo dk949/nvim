be144eda92b1d6f2bae1fe4ef9cf9e1c573e69ba
dk949 <56653556+dk949@users.noreply.github.com>
2022-11-27 02:40:33 +0000

    added custom lsp icons and always expanded signcolumn

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,4 +1,13 @@
 function LSPConfigFn()
+
+    local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+    for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    end
+
+    vim.opt.signcolumn = "yes"
+
     local luasnip = require 'luasnip'
 
     local cmp = require 'cmp'
