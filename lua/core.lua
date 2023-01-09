fe386c816ab2b28368bb1afb82d4f62e951193db
dk949 <56653556+dk949@users.noreply.github.com>
2023-01-09 18:51:08 +0200

    changed some core settings

--- lua/core.lua
+++ lua/core.lua
@@ -4,50 +4,56 @@ local api = vim.api
 vim.g.mapleader = dk949.leader
 
 -- Which files are considered code
-dk949.codeft = {"ada", "asm", "asm68k", "asm_ca65", "asmh8300", "autohotkey", "automake", "awk", "b", "basic",
-"c", "chaiscript", "clean", "clojure", "cmake", "cobol", "cpp", "cs", "csh", "css", "cuda", "d",
-"dart", "django", "dockerfile", "dosbatch", "dosini", "doxygen", "eiffel", "elixir", "elm",
-"erlang", "fasm", "forth", "fortran", "freebasic", "gdscript", "gdshader", "go", "groovy",
-"haml", "haskell", "html", "j", "java", "javascript", "javascriptreact", "json", "jsonc",
-"julia", "lisp", "lua", "m4", "make", "masm", "meson", "modula2", "modula3", "nasm", "ninja",
-"objc", "objcpp", "ocaml", "octave", "pascal", "perl", "php", "prolog", "ps1", "python", "r",
-"racket", "raku", "ruby", "rust", "sass", "scala", "scheme", "sh", "simula", "sql", "svg",
-"swift", "tcl", "tcsh", "vhdl", "vim", "vue", "yacc", "yaml", "zsh", "zig"}
+dk949.codeft = { "ada", "asm", "asm68k", "asm_ca65", "asmh8300", "autohotkey", "automake", "awk", "b", "basic",
+    "c", "chaiscript", "clean", "clojure", "cmake", "cobol", "cpp", "cs", "csh", "css", "cuda", "d",
+    "dart", "django", "dockerfile", "dosbatch", "dosini", "doxygen", "eiffel", "elixir", "elm",
+    "erlang", "fasm", "forth", "fortran", "freebasic", "gdscript", "gdshader", "go", "groovy",
+    "haml", "haskell", "html", "j", "java", "javascript", "javascriptreact", "json", "jsonc",
+    "julia", "lisp", "lua", "m4", "make", "masm", "meson", "modula2", "modula3", "nasm", "ninja",
+    "objc", "objcpp", "ocaml", "octave", "pascal", "perl", "php", "prolog", "ps1", "python", "r",
+    "racket", "raku", "ruby", "rust", "sass", "scala", "scheme", "sh", "simula", "sql", "svg",
+    "swift", "tcl", "tcsh", "vhdl", "vim", "vue", "yacc", "yaml", "zsh", "zig" }
 
-dk949.noncodeft = {"markdown", "tex", "plaintex", "vimwiki"}
+dk949.noncodeft = { "markdown", "tex", "plaintex", "vimwiki" }
 
 -- Turning on the filetype plugin
-vim.cmd[[filetype plugin on]]
-vim.cmd[[filetype plugin indent on]]
+vim.cmd [[filetype plugin on]]
+vim.cmd [[filetype plugin indent on]]
 
 -- Hybrid numbering in normal mode and normal numbering in insert mode
-local relNum = setter {relativenumber = true, number = true}
-local noRelNum = setter {relativenumber = false, number = true}
+local relNum = setter { relativenumber = true, number = true }
+local noRelNum = setter { relativenumber = false, number = true }
 augroup("NumberGroup", {
-    InsertLeave = {callback = relNum},
-    InsertEnter = {callback = setter {relativenumber = false}},
+    InsertLeave = { callback = relNum },
+    InsertEnter = { callback = setter { relativenumber = false } },
 })
 relNum()
 
 --  Terminal stuff
 augroup("TerminalGroup", {
-    TermOpen = {callback = setter {relativenumber = false, number = false}},
-    [{"BufWinEnter","WinEnter"}] = {pattern = "term://*", command = "startinsert"}
+    TermOpen = { callback = setter { relativenumber = false, number = false } },
+    [{ "BufWinEnter", "WinEnter" }] = { pattern = "term://*", command = "startinsert" }
 })
-
+-- vim.opt.formatoptions:remove({'c','r','o', 't'})
 -- Remove unnecessary auto formatting
 augroup("FormatOptsGroup", {
-    BufEnter = { callback = function() vim.opt.formatoptions:remove({'c','r','o', 't'}) end}
+    BufEnter = { callback = function()
+        vim.opt.formatoptions:remove('c')
+        vim.opt.formatoptions:remove('r')
+        vim.opt.formatoptions:remove('o')
+        vim.opt.formatoptions:remove('t')
+    end
+    }
 })
 
 -- No whitespace at the ned of lines
 augroup("NoTrailingSpacesGroup", {
-    BufWritePre = {command = [[%s/\s\+$//e]]},
+    BufWritePre = { command = [[%s/\s\+$//e]] },
 })
 
 augroup("RememberFileGroup", {
-    [{"VimLeave", "BufLeave"}] = {callback = function() if dk949.nomkview then dk949.nomkview = false else vim.cmd[[silent! mkview]]end end},
-    [{"VimEnter", "BufEnter"}] = {command = [[silent! loadview]]}
+    BufWritePre = { callback = function() if dk949.nomkview then dk949.nomkview = false else vim.cmd [[silent! mkview]] end end },
+    BufEnter = { command = [[silent! loadview]] }
 })
 
 -- setting tabsize
