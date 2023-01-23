0c961f24f6f9852af613d38b6211ea5186367d80
dk949 <56653556+dk949@users.noreply.github.com>
2023-01-23 23:44:28 +0000

    added haskell plugin

---
+++ lua/plugins/haskell.lua
@@ -0,0 +1,14 @@
+return {
+    "neovimhaskell/haskell-vim",
+    ft = { "haskell" },
+    config = function()
+        vim.g.haskell_backpack = 1 -- to enable highlighting of backpack keywords
+        vim.g.haskell_classic_highlighting = 0 -- more traditional highlighting
+        vim.g.haskell_enable_arrowsyntax = 1 -- to enable highlighting of `proc`
+        vim.g.haskell_enable_pattern_synonyms = 1 -- to enable highlighting of `pattern`
+        vim.g.haskell_enable_quantification = 1 -- to enable highlighting of `forall`
+        vim.g.haskell_enable_recursivedo = 1 -- to enable highlighting of `mdo` and `rec`
+        vim.g.haskell_enable_static_pointers = 1 -- to enable highlighting of `static`
+        vim.g.haskell_enable_typeroles = 1 -- to enable highlighting of type roles
+    end
+}
--- lua/plugins/package.lua
+++ lua/plugins/package.lua
@@ -43,6 +43,7 @@ return require("packer").startup(function()
     localUse "markdown"
     localUse "zig"
     localUse "rust"
+    localUse "haskell"
 
     localUse "treesitter"
 
