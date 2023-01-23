a85ac30748a38a32859e2de0e7f5862545783ad7
dk949 <56653556+dk949@users.noreply.github.com>
2023-01-23 22:50:56 +0000

    added config to cxx_highlight

--- lua/plugins/lsp/cxx_heighlight.lua
+++ lua/plugins/lsp/cxx_heighlight.lua
@@ -2,5 +2,10 @@
 return {
     "jackguo380/vim-lsp-cxx-highlight",
     ft = { "c", "cpp" },
-    after = { "lspconfig" }
+    after = { "lspconfig" },
+    config = function()
+        vim.g.cpp_class_scope_highlight = 1
+        vim.g.cpp_member_variable_highlight = 1
+        vim.g.cpp_class_decl_highlight = 1
+    end,
 }
