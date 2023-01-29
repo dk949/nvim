9fa014ef5b6e2795524802799716e3acb3487fd6
dk949 <56653556+dk949@users.noreply.github.com>
2023-01-29 21:32:32 +0000

    ignore unknown at rules in css

--- lua/lang_features/cssls.lua
+++ lua/lang_features/cssls.lua
@@ -1,10 +1,19 @@
 return {
     server = require("utils").lspSetupCreate("cssls",
         function(capabilities, on_attach)
+            local settings = {
+                validate = false,
+                unknownAtRules = "ignore",
+            }
             require("lspconfig").cssls.setup {
                 capabilities = capabilities,
                 on_attach = on_attach,
-                completion = { callSnippet = "Replace" }
+                completion = { callSnippet = "Replace" },
+                settings = {
+                    css  = settings,
+                    less = settings,
+                    scss = settings,
+                }
             }
         end
     ),
