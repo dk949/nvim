080b1b3de4885590f9f692b2e070a1d0053115fe
dk949 <56653556+dk949@users.noreply.github.com>
2023-02-12 23:35:14 +0000

    added ca65 assembly as a filetype

---
+++ ftdetect/cc65.vim
@@ -0,0 +1,1 @@
+au BufNewFile,BufRead *.s65,*.h65 set filetype=asm_ca65
--- lua/plugins/asm_ca65.lua
+++ lua/plugins/asm_ca65.lua
@@ -1,2 +1,5 @@
 -- https://github.com/maxbane/vim-asm_ca65
-return { 'maxbane/vim-asm_ca65' }
+return {
+    'maxbane/vim-asm_ca65',
+    ft = { "asm_ca65" }
+}
