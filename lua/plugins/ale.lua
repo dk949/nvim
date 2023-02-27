cf802aacd9ca368e563cf070d7349cac44f7fba6
dk949 <56653556+dk949@users.noreply.github.com>
2023-02-27 10:24:02 +0000

    enable ale for shellcheck and nothing else

--- lua/plugins/ale.lua
+++ lua/plugins/ale.lua
@@ -1,3 +1,7 @@
 vim.g.ale_disable_lsp = true
 vim.g.ale_virtualtext_cursor = false -- it's distracting in combination with the lsp virtual text
+vim.g.ale_linters_explicit = true -- Only run linters named in ale_linters settings.
+vim.g.ale_linters = {
+    sh = { 'shellcheck' },
+}
 return { "dense-analysis/ale" }
