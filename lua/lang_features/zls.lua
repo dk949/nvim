012567a2f0e67de0c6314424f166339216f33781
dk949 <56653556+dk949@users.noreply.github.com>
2023-06-03 15:12:07 +0100

    zls do not parse errors, do not highlight comments

--- lua/lang_features/zls.lua
+++ lua/lang_features/zls.lua
@@ -1,1 +1,12 @@
-return require("utils").makeDefaultLspCounfig("zls", { install = false })
+vim.g.zig_fmt_parse_errors = false
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    pattern = "*",
+    callback = function() vim.api.nvim_set_hl(0, '@lsp.type.comment.zig', {}) end
+});
+return require("utils").makeDefaultLspCounfig("zls", {
+    install = false,
+    settings = {
+        enable_inlay_hints = true
+    },
+})
