14b901c6370a23dfc808e61f8cb5b2cd456fb048
dk949 <56653556+dk949@users.noreply.github.com>
2023-12-14 14:13:47 +0000

    vim-markdown options

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -3,11 +3,20 @@ return {
     "plasticboy/vim-markdown",
     ft = "markdown",
     config = function()
+        vim.g.vim_markdown_new_list_item_indent    = 0
+        vim.opt_local.conceallevel                 = 2
+
         vim.g.vim_markdown_auto_insert_bullets     = true
         vim.g.vim_markdown_folding_disabled        = true
         vim.g.vim_markdown_follow_anchor           = true
-        vim.g.vim_markdown_new_list_item_indent    = 0
         vim.g.vim_markdown_no_default_key_mappings = false
         vim.g.vim_markdown_strikethrough           = true
+        vim.g.vim_markdown_toc_autofit             = true
+        vim.g.vim_markdown_emphasis_multiline      = false
+        vim.g.vim_markdown_math                    = true
+        vim.g.vim_markdown_conceal_code_blocks     = true
+        vim.g.vim_markdown_follow_anchor           = true
+        vim.g.vim_markdown_strikethrough           = true
+        vim.g.vim_markdown_conceal_code_blocks     = false
     end,
 }
