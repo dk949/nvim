ef7b67274ff8e4ed7d55bbce6f9cf0e855d14dc1
dk949 <56653556+dk949@users.noreply.github.com>
2024-05-15 14:19:59 +0100

    metter documentation of the format arguments

--- lua/lang_features/package.lua
+++ lua/lang_features/package.lua
@@ -127,45 +127,45 @@ M.formatoptions = (function()
     local out = {}
 
 
---- t   Auto-wrap text using 'textwidth'
----
---- c   Auto-wrap comments using 'textwidth', inserting the current comment
----     leader automatically.
----
---- r   Automatically insert the current comment leader after hitting
----     <Enter> in Insert mode.
----
---- /   When 'o' is included: do not insert the comment leader for a //
----     comment after a statement, only when // is at the start of the line.
----
---- q   Allow formatting of comments with "gq".
----     Note that formatting will not change blank lines or lines containing
----     only the comment leader.  A new paragraph starts after such a line,
----     or when the comment leader changes.
----
---- w    Trailing white space indicates a paragraph continues in the next line.
----     A line that ends in a non-white character ends a paragraph.
----
---- a   Automatic formatting of paragraphs.  Every time text is inserted or
----     deleted the paragraph will be reformatted.  See |auto-format|.
----     When the 'c' flag is present this only happens for recognized
----     comments.
----
---- n   When formatting text, recognize numbered lists.  This actually uses
----     the 'formatlistpat' option, thus any kind of list can be used.  The
----     indent of the text after the number is used for the next line.  The
----     default is to find a number, optionally followed by '.', ':', ')',
----     ']' or '}'.  Note that 'autoindent' must be set too.  Doesn't work
----     well together with "2".
----
---- l   Long lines are not broken in insert mode: When a line was longer than
----     'textwidth' when the insert command started, Vim does not
----     automatically format it.
----
---- 1    Don't break a line after a one-letter word.  It's broken before it
----     instead (if possible).
----
---- j   Where it makes sense, remove a comment leader when joining lines.
+    --- t   Auto-wrap text using 'textwidth'
+    ---
+    --- c   Auto-wrap comments using 'textwidth', inserting the current comment
+    ---     leader automatically.
+    ---
+    --- r   Automatically insert the current comment leader after hitting
+    ---     <Enter> in Insert mode.
+    ---
+    --- /   When 'o' is included: do not insert the comment leader for a //
+    ---     comment after a statement, only when // is at the start of the line.
+    ---
+    --- q   Allow formatting of comments with "gq".
+    ---     Note that formatting will not change blank lines or lines containing
+    ---     only the comment leader.  A new paragraph starts after such a line,
+    ---     or when the comment leader changes.
+    ---
+    --- w    Trailing white space indicates a paragraph continues in the next line.
+    ---     A line that ends in a non-white character ends a paragraph.
+    ---
+    --- a   Automatic formatting of paragraphs.  Every time text is inserted or
+    ---     deleted the paragraph will be reformatted.  See |auto-format|.
+    ---     When the 'c' flag is present this only happens for recognized
+    ---     comments.
+    ---
+    --- n   When formatting text, recognize numbered lists.  This actually uses
+    ---     the 'formatlistpat' option, thus any kind of list can be used.  The
+    ---     indent of the text after the number is used for the next line.  The
+    ---     default is to find a number, optionally followed by '.', ':', ')',
+    ---     ']' or '}'.  Note that 'autoindent' must be set too.  Doesn't work
+    ---     well together with "2".
+    ---
+    --- l   Long lines are not broken in insert mode: When a line was longer than
+    ---     'textwidth' when the insert command started, Vim does not
+    ---     automatically format it.
+    ---
+    --- 1    Don't break a line after a one-letter word.  It's broken before it
+    ---     instead (if possible).
+    ---
+    --- j   Where it makes sense, remove a comment leader when joining lines.
 
 
     for _, lang in ipairs(text) do out[lang] = "tcr/qn1jl" end
