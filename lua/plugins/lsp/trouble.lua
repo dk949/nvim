f18c711f17ddbe8c23031012397afb0c7ad5b6a5
dk949 <56653556+dk949@users.noreply.github.com>
2024-07-07 01:48:18 +0100

    updated trouble config

--- lua/plugins/lsp/trouble.lua
+++ lua/plugins/lsp/trouble.lua
@@ -8,7 +8,6 @@ return {
             height = 15, -- height of the trouble list when position is top or bottom
             position = "bottom", -- position of the list can be: bottom, top, left, right
             width = 50, -- width of the list when position is left or right
-            icons = true, -- use devicons for filenames
             mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
             action_keys = { -- key mappings for actions in the trouble list
                 hover = "<leader>mi", -- opens a small popup with the full multiline message
