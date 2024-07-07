467cb6090c5764f4b6d5afd89576986dc6c84c52
dk949 <56653556+dk949@users.noreply.github.com>
2024-07-07 01:28:26 +0100

    use emmet in xml

--- lua/plugins/emmet.lua
+++ lua/plugins/emmet.lua
@@ -2,6 +2,6 @@ vim.g.user_emmet_leader_key = '<C-B>'
 
 return {
     "mattn/emmet-vim",
-    ft = { "html", "css", "javascriptreact", "typescriptreact"}
+    ft = { "html", "xml", "svg", "css", "javascriptreact", "typescriptreact"}
 
 }
