a4a3eeb5c3fe3e8e2c86a16900853447b732a667
dk949 <56653556+dk949@users.noreply.github.com>
2024-09-01 17:33:07 +0100

    bullets for mdx

--- lua/plugins/bullets.lua
+++ lua/plugins/bullets.lua
@@ -1,6 +1,6 @@
 -- https://github.com/bullets-vim/bullets.vim
 
-vim.g.bullets_enabled_file_types = { 'markdown' }
+vim.g.bullets_enabled_file_types = { 'markdown', 'mdx' }
 return {
     "bullets-vim/bullets.vim",
     -- ft = { "markdown" },
