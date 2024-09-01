97de0da1d2ecc43ae960dbdbea4b672483a43515
dk949 <56653556+dk949@users.noreply.github.com>
2024-09-01 17:25:14 +0100

    make tailwind work in haskell

--- lua/lang_features/tailwindcss.lua
+++ lua/lang_features/tailwindcss.lua
@@ -1,4 +1,9 @@
 return require("utils").makeDefaultLspCounfig("tailwindcss", {
+    filetypes = { "aspnetcorerazor", "astro", "astro-markdown", "blade", "clojure", "django-html", "htmldjango", "edge",
+        "eelixir", "elixir", "ejs", "erb", "eruby", "gohtml", "gohtmltmpl", "haml", "handlebars", "haskell", "hbs",
+        "html", "htmlangular", "html-eex", "heex", "jade", "leaf", "liquid", "markdown", "mdx", "mustache", "njk",
+        "nunjucks", "php", "razor", "slim", "twig", "css", "less", "postcss", "sass", "scss", "stylus", "sugarss",
+        "javascript", "javascriptreact", "reason", "rescript", "typescript", "typescriptreact", "vue", "svelte", "templ" },
     settings = {
         tailwindCSS = {
             classAttributes = {
@@ -7,7 +12,11 @@ return require("utils").makeDefaultLspCounfig("tailwindcss", {
                 "ngClass",
                 "class:list",
                 "style",
-            }
+            },
+            -- includeLanguages = { haskell = "html" },
+            experimental = {
+                classRegex = { "class_ \"([^\"]*)" },
+            },
         }
     }
 })
