37c2cc5f565019675bb2b1749a16ca8161a3f9b2
dk949 <56653556+dk949@users.noreply.github.com>
2024-11-26 02:01:54 +0000

    inlay hints for tsserver

--- lua/lang_features/tsserver.lua
+++ lua/lang_features/tsserver.lua
@@ -1,1 +1,16 @@
-return require("utils").makeDefaultLspCounfig("tsserver")
+return require("utils").makeDefaultLspCounfig("tsserver", {
+    -- settings = {
+    init_options = {
+        preferences = {
+            includeInlayParameterNameHints = "all",
+            includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayVariableTypeHints = true,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            includeInlayEnumMemberValueHints = true,
+            importModuleSpecifierPreference = 'non-relative'
+        },
+        -- }
+    }
+})
