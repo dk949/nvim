f6bb3abac453119f2949ad8fc00134934ee75a4a
dk949 <56653556+dk949@users.noreply.github.com>
2025-04-10 23:15:56 +0100

    removed call to reprecated sign_define

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -3,11 +3,22 @@ return {
     after = { "snip", "cmp" },
     as = "lspconfig",
     config = function()
-        local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-        end
+        vim.diagnostic.config({
+            signs = {
+                text  = {
+                    [vim.diagnostic.severity.ERROR] = "󰅚 ",
+                    [vim.diagnostic.severity.WARN] = "󰀪 ",
+                    [vim.diagnostic.severity.INFO] = " ",
+                    [vim.diagnostic.severity.HINT] = "󰌶 ",
+                },
+                numhl = {
+                    [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+                    [vim.diagnostic.severity.WARN]  = "DiagnosticSignWarn",
+                    [vim.diagnostic.severity.INFO]  = "DiagnosticSignHint",
+                    [vim.diagnostic.severity.HINT]  = "DiagnosticSignInfo",
+                },
+            }
+        })
 
         vim.opt.signcolumn = "yes"
         dk949.lsp_loaded = true
