d8c650676209bab14aa17384eb10903266c13418
dk949 <56653556+dk949@users.noreply.github.com>
2025-05-08 03:46:31 +0100

    trying to use the new diagnostics API
    TODO: doesn't work with ALE

--- lua/core.lua
+++ lua/core.lua
@@ -103,5 +103,27 @@ vim.opt.smoothscroll = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
--- Mouse
-vim.opt.mouse = dk949.mouse
+-- disable folds by default
+vim.opt.foldenable = false
+
+-- Diagnostics
+-- TODO(dk949): Make this work with ALE
+vim.diagnostic.config {
+    underline = true,
+    virtual_lines = false, -- consider re-enabling
+    update_in_insert = false,
+    signs = {
+        text  = {
+            [vim.diagnostic.severity.ERROR] = "󰅚 ",
+            [vim.diagnostic.severity.WARN] = "󰀪 ",
+            [vim.diagnostic.severity.INFO] = " ",
+            [vim.diagnostic.severity.HINT] = "󰌶 ",
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+            [vim.diagnostic.severity.WARN]  = "DiagnosticSignWarn",
+            [vim.diagnostic.severity.INFO]  = "DiagnosticSignHint",
+            [vim.diagnostic.severity.HINT]  = "DiagnosticSignInfo",
+        },
+    }
+}
