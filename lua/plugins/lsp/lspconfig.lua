16c920ee622433aa1ce7dc8f8cc17e59a86789c0
dk949 <56653556+dk949@users.noreply.github.com>
2025-05-08 03:54:49 +0100

    removed diagnostics config from the lspconfig file

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -3,23 +3,6 @@ return {
     after = { "snip", "cmp" },
     as = "lspconfig",
     config = function()
-        vim.diagnostic.config({
-            signs = {
-                text  = {
-                    [vim.diagnostic.severity.ERROR] = "󰅚 ",
-                    [vim.diagnostic.severity.WARN] = "󰀪 ",
-                    [vim.diagnostic.severity.INFO] = " ",
-                    [vim.diagnostic.severity.HINT] = "󰌶 ",
-                },
-                numhl = {
-                    [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-                    [vim.diagnostic.severity.WARN]  = "DiagnosticSignWarn",
-                    [vim.diagnostic.severity.INFO]  = "DiagnosticSignHint",
-                    [vim.diagnostic.severity.HINT]  = "DiagnosticSignInfo",
-                },
-            }
-        })
-
         vim.opt.signcolumn = "yes"
         dk949.lsp_loaded = true
     end,
