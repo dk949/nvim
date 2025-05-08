6b01bab6cc7dc4bb2f191b3478eff1a9211af1fa
dk949 <56653556+dk949@users.noreply.github.com>
2025-05-08 12:53:01 +0100

    mouse is alway on now

--- lua/commands.lua
+++ lua/commands.lua
@@ -151,13 +151,11 @@ utils.addAbrev("gcan", "GitCommitAmmendNoEdit")
 
 api.nvim_create_user_command("UserFriendly", function(args)
     if args.bang then
-        vim.opt.mouse = ""
         vim.keymap.set({ 'n', 'v', 'i' }, "<left>", "<nop>", { silent = true })
         vim.keymap.set({ 'n', 'v', 'i' }, "<right>", "<nop>", { silent = true })
         vim.keymap.set({ 'n', 'v', 'i' }, "<up>", "<nop>", { silent = true })
         vim.keymap.set({ 'n', 'v', 'i' }, "<down>", "<nop>", { silent = true })
     else
-        vim.opt.mouse = "a"
         vim.keymap.set({ 'n', 'v', 'i' }, "<left>", "h", { silent = true })
         vim.keymap.set({ 'n', 'v', 'i' }, "<right>", "l", { silent = true })
         vim.keymap.set({ 'n', 'v', 'i' }, "<up>", "k", { silent = true })
