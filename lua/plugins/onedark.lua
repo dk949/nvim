a462a0bca9be7e5821db514de5db342c25e0319d
dk949 <56653556+dk949@users.noreply.github.com>
2025-05-10 16:14:17 +0100

    cursor colors

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -32,7 +32,8 @@ return { {
 
             colors = {}, -- Override default colors
             highlights = {
-                Title = { bg = '$bg1'},
+                Title      = { bg = '$bg1' },
+                Cursor     = { bg = "$fg", fg = "$bg0", fmt="NONE" },
                 SpellBad   = { fg = '$red', fmt = 'underdouble' },
                 SpellCap   = { fg = '$cyan' },
                 SpellLocal = { fg = "$orange", fmt = 'underline' },
@@ -44,6 +45,7 @@ return { {
                 background = true, -- use background color for virtual text
             },
         }
+        vim.opt.guicursor="n-v-c-sm:block-Cursor,i-ci-ve:ver25-Cursor,r-cr-o:hor20-Cursor,t:block-blinkon500-blinkoff500-TermCursor"
         require('onedark').load()
     end
 } }
