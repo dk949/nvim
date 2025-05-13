3ccb38605011ced20efb562548cf70db3e5fd160
dk949 <56653556+dk949@users.noreply.github.com>
2025-05-13 01:10:15 +0100

    make sure to only load ftplugin once and install all required servers

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,1 +1,12 @@
+if vim.b.ftp_is_done then return end
+
 vim.lsp.enable("lua_ls")
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        local reg = require("mason-registry")
+        if reg.is_installed("lua-language-server") then return end
+        local pkg = reg.get_package("lua-language-server")
+        pkg:install()
+    end
+})
+vim.b.ftp_is_done = true
