1e859fc9f710c83bdd2c126209299998470e55a1
dk949 <56653556+dk949@users.noreply.github.com>
2025-05-14 00:32:40 +0100

    cleaned up theme setup

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -1,5 +1,5 @@
 local keymap = require("config.keymap")
-local colorscheme = {
+local colorschemes = {
     dark = "habamax",
     light = "zellner",
 }
@@ -9,13 +9,13 @@ local function invert(col)
     if col == "dark" then return "light" else return "dark" end
 end
 
-
-vim.cmd.colorscheme("habamax")
-vim.opt.guicursor =
-"n-v-c-sm:block-Cursor,i-ci-ve:ver25-Cursor,r-cr-o:hor20-Cursor,t:block-blinkon500-blinkoff500-TermCursor"
+vim.cmd.colorscheme(colorschemes[current])
 
 vim.opt.fillchars:append({ eob = " " })
 keymap.color:defaultApply { colortoggle = function()
     current = invert(current)
-    vim.cmd.colorscheme(colorscheme[current])
+    vim.cmd.colorscheme(colorschemes[current])
 end }
+
+vim.opt.guicursor =
+"n-v-c-sm:block-Cursor,i-ci-ve:ver25-Cursor,r-cr-o:hor20-Cursor,t:block-blinkon500-blinkoff500-TermCursor"
