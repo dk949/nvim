3f506a8e31890db1d585d988bb7dbb242b72f8bc
dk949 <56653556+dk949@users.noreply.github.com>
2025-05-13 22:56:11 +0100

    always have signcolumn to avoid constantly shifting text

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,6 +1,7 @@
 local utils = require "utils"
 local lsp_utils = require "utils.lsp"
 utils.ftplugin(function()
+    vim.opt_local.signcolumn = "yes"
     lsp_utils.enableLsp({
         config = "lua_ls",
         mason = "lua-language-server",
