5cfb23908810eacc18411f15e87e5cd8cbf846c6
dk949 <56653556+dk949@users.noreply.github.com>
2025-05-15 01:20:49 +0100

    added switch for dark/light mode

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -1,4 +1,17 @@
 local keymap = require("config.keymap")
+local utils = require("utils")
+
+utils.withAugroup("color", function(grp)
+    vim.api.nvim_create_autocmd("ColorScheme", {
+        callback = function()
+            local normal_float = vim.api.nvim_get_hl(0, { name = "NormalFloat", create = false, link = false })
+            vim.api.nvim_set_hl(0, "FloatBorder", normal_float)
+            normal_float.bold = true
+            vim.api.nvim_set_hl(0, "FloatTitle", normal_float)
+        end
+    })
+end)
+
 local colorschemes = {
     dark = "habamax",
     light = "zellner",
@@ -16,6 +29,7 @@ keymap.color:defaultApply { colortoggle = function()
     current = invert(current)
     vim.cmd.colorscheme(colorschemes[current])
 end }
+
 
 vim.opt.guicursor =
 "n-v-c-sm:block-Cursor,i-ci-ve:ver25-Cursor,r-cr-o:hor20-Cursor,t:block-blinkon500-blinkoff500-TermCursor"
