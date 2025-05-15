c039c768fa14588731660eeca57bbb69383e2d67
dk949 <56653556+dk949@users.noreply.github.com>
2025-05-15 02:11:29 +0100

    autotrigger completion, but do not select anything

--- lua/config/options/complete.lua
+++ lua/config/options/complete.lua
@@ -1,12 +1,13 @@
 local utils = require("utils")
 vim.opt.completeopt:remove("menu")
 vim.opt.completeopt:append("menuone")
+vim.opt.completeopt:append("noselect")
 utils.withAugroup("completions", function(grp)
     vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
             local client = vim.lsp.get_client_by_id(args.data.client_id)
             if client:supports_method('textDocument/completion') then
-                vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
+                vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
             end
         end,
         group = grp
