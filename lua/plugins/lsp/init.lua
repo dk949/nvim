4ef0bc360c0164822ec734f565eb17bab33ef5c1
dk949 <56653556+dk949@users.noreply.github.com>
2025-05-20 03:04:06 +0100

    reformatted

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,5 +1,5 @@
 local ft = { "lua" }
 return {
     { "neovim/nvim-lspconfig", lazy = false },
-    { "mason-org/mason.nvim",  ft = ft,   opts = {} },
+    { "mason-org/mason.nvim",  ft = ft,     opts = {} },
 }
