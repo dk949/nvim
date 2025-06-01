ea2d694cb81d4193921c96d96d7ea95cecc0165f
dk949 <56653556+dk949@users.noreply.github.com>
2025-06-01 02:50:16 +0100

    added tabular

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,4 +4,5 @@ return {
     { 'stevearc/oil.nvim',           lazy = false, opts = require("config.plugins.oil") },
     { "lewis6991/gitsigns.nvim",     lazy = false, opts = require("config.plugins.gitsigns") },
     { "willothy/flatten.nvim",       lazy = false, opts = require("config.plugins.flatten"), priority = 1000, },
+    { "godlygeek/tabular",           lazy = true , cmd="Tabularize"},
 }
