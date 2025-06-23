837d49ee0f16e43ee8b3724e1ca9bddaceb863f5
dk949 <56653556+dk949@users.noreply.github.com>
2025-06-23 22:24:27 +0100

    fixed diagnostics
    Closes #48

--- lua/config/options/diag.lua
+++ lua/config/options/diag.lua
@@ -2,8 +2,10 @@
 -- TODO(dk949): Make this work with ALE
 vim.diagnostic.config {
     underline = true,
-    virtual_lines = false, -- consider re-enabling
+    virtual_text = { severity = "ERROR" },
+    virtual_lines = false,
     update_in_insert = false,
+    severity_sort = { reverse = false },
     signs = {
         text  = {
             [vim.diagnostic.severity.ERROR] = "󰅚 ",
