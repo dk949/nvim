3d7620f81f3e48f15e1374b55a5aeaf56d215120
dk949 <56653556+dk949@users.noreply.github.com>
2025-06-23 15:30:05 +0100

    don't print the spurious error when failing to delete autocmd

--- lua/config/common/ws.lua
+++ lua/config/common/ws.lua
@@ -20,8 +20,8 @@ end
 
 function M.undo()
     if not vim.b.no_trailing_ws then return end
-    local ok, err = xpcall(vim.api.nvim_del_autocmd, debug.traceback, vim.b.no_trailing_ws)
-    if not ok then log.warn(err) end
+    -- TODO(dk949): Fix this properly
+    pcall(vim.api.nvim_del_autocmd, vim.b.no_trailing_ws)
     vim.b.no_trailing_ws = nil
 end
 
