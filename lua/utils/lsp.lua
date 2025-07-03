d48b4a00e7da3c04fb9a6682c487e5348683d57f
dk949 <56653556+dk949@users.noreply.github.com>
2025-07-03 13:41:07 +0100

    allow lsp overrides to be specified

--- lua/utils/lsp.lua
+++ lua/utils/lsp.lua
@@ -4,6 +4,8 @@ local lsp = vim.lsp
 
 local enabled_lsps = {}
 
+---@alias Override vim.lsp.Config|fun(_:vim.lsp.Config?):vim.lsp.Config
+
 ---ensure an LSP server is installed with mason
 ---@param name string|false
 local function ensureInstalled(name)
@@ -17,7 +19,19 @@ end
 
 ---Load all required plugins and ensure the LSP is installed
 ---@param name {config:string, mason:(string|boolean)?}
-local function setupLSP(name)
+---@param override Override?
+local function setupLSP(name, override)
+    print("here")
+    if override then
+        if type(override) == "table" then
+            vim.lsp.config(name.config, override)
+        else
+            vim.lsp.config[name.config] = override(vim.lsp.config[name.config])
+        end
+    end
+    vim.lsp.config(name.config, {
+        capabilities = require('cmp_nvim_lsp').default_capabilities()
+    })
     lazy.load({
         plugins = {
             "nvim-lspconfig",
@@ -30,21 +44,19 @@ local function setupLSP(name)
     })
     vim.cmd [[doautocmd FileType]]
     ensureInstalled(name.mason)
-    vim.lsp.config("*", {
-        capabilities = require('cmp_nvim_lsp').default_capabilities()
-    })
 end
 
 
 ---@param name {config:string, mason:(string|boolean)?}|string
-function M.enableLsp(name)
+---@param override Override?
+function M.enableLsp(name, override)
     -- XXX: will result in infinite recursion without this due to `doautocmd FileType`
     if type(name) == "string" then name = { config = name, mason = name } end
     if enabled_lsps[name.config] then return end
     enabled_lsps[name.config] = true
     if name.mason == nil then name.mason = name.config end
     vim.lsp.enable(name.config)
-    setupLSP(name)
+    setupLSP(name, override)
 end
 
 function M.toggleInlay()
