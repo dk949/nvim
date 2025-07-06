945efad0858120b49edbfb9a5583a0f36c7e3348
dk949 <56653556+dk949@users.noreply.github.com>
2025-07-06 18:18:15 +0100

    make sure indent-blankline is only enabled for buffers that require it

--- lua/config/common/feat.lua
+++ lua/config/common/feat.lua
@@ -4,6 +4,7 @@ local M = {}
 M["@indentBlankline"] = function(yes)
     if not yes then return end
     lazy.load({ plugins = { "indent-blankline.nvim" } })
+    require "ibl".setup_buffer(vim.fn.bufnr(), {enabled = true})
 end
 
 M["@trailingWS"] = function(yes)
--- lua/config/plugins/indent_blankline.lua
+++ lua/config/plugins/indent_blankline.lua
@@ -1,5 +1,6 @@
 local highlight = { "Special", "Comment", }
 return {
+    enabled = false,
     indent = { highlight = highlight },
     scope = { enabled = false }
 }
