9536e992290055aedf01f4aeb118c7998f9e05a3
dk949 <56653556+dk949@users.noreply.github.com>
2025-07-21 22:36:44 +0100

    plugin(lazydev): add an override for when plugin is enabled

--- lua/config/plugins/lazydev.lua
+++ lua/config/plugins/lazydev.lua
@@ -1,6 +1,8 @@
 return {
     opts = {},
     cond = function()
+        -- Enable lazydev with nvim --cmd 'let g:enable_lazydev=1'
+        if vim.g.enable_lazydev then return true end
         local conf = vim.fs.normalize(vim.fs.joinpath(vim.env.XDG_CONFIG_HOME or "~/.config", "nvim"))
         local cwd = vim.fs.normalize(vim.env.PWD)
         return vim.startswith(cwd, conf)
