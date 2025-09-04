166628cbafc5d0048931903a7695a1042d40a50c
dk949 <56653556+dk949@users.noreply.github.com>
2025-09-04 01:55:22 +0100

    plugin(neorg): added image and export support

--- lua/config/plugins/neorg.lua
+++ lua/config/plugins/neorg.lua
@@ -6,9 +6,12 @@ return {
                 icons = { todo = { uncertain = { icon = "" } } },
             },
         },
+        ["core.integrations.image"] = {},
+        ["core.latex.renderer"] = {},
+        ["core.export"] = {},
         ["core.dirman"] = {
             config = {
-                workspaces = { notes = "~/Uni/notes", },
+                workspaces = { notes = "~/Uni/notes/", },
                 default_workspace = "notes",
                 open_last_workspace = false, -- maybe?
             },
