d537ab4bf453bfe9bafe7cb3237d3fafc3539d7a
dk949 <56653556+dk949@users.noreply.github.com>
2025-09-04 01:52:04 +0100

    command(Make): now uses makeprg

--- lua/commands/make.lua
+++ lua/commands/make.lua
@@ -1,12 +1,33 @@
 local log = require("utils.log")
-local cut = require("commands.utils")
 local command = vim.api.nvim_create_user_command
-command("Make", function()
-    vim.system({"make"}, {}, function (out)
+command("Make", function(arg)
+    ---@type string
+    local makeprg = vim.opt_local.makeprg:get()
+    if makeprg:find([[\|]]) or makeprg:find('&') then
+        log.warn("makeprg too complex: ", makeprg)
+        return
+    end
+    local replaced = false
+    local prog = vim.iter(vim.split(makeprg, " ", { plain = true, trimempty = true }))
+        :map(function(p)
+            if p == "$*" then
+                replaced = true
+                return arg.fargs
+            else
+                return p
+            end
+        end)
+        :flatten()
+        :totable()
+    if not replaced then
+        prog = vim.list_extend(prog, arg.fargs)
+    end
+
+    vim.system(prog, nil, function(out)
         if out.code ~= 0 or out.signal ~= 0 then
-            log.sched.error({"Make error:", out.stderr})
+            log.sched.error({ "Make error:", out.stderr })
         else
             log.sched.info("Make success!")
         end
     end)
-end, {})
+end, { nargs = "*" })
