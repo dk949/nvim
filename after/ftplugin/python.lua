c172d950e863280b3127a2d10ac80b25a4bb6b57
dk949 <56653556+dk949@users.noreply.github.com>
2025-09-14 22:08:18 +0100

    lang(python): use formatprg for python

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -24,7 +24,8 @@ end
 
 require "utils".ftplugin(
     require "config.common".prog:with {
-        formatexpr = [[v:lua.require("config.common.formatting").pythonExpr()]],
+        -- formatexpr = [[v:lua.require("config.common.formatting").pythonExpr()]],
+        formatprg = require("config.common.formatting").pythonPrg,
     },
     function()
         require "utils.lsp".enableLspTools("basedpyright", { snippets = snippets })
