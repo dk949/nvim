b067654409a4e58d8afee55373f24f59d8eb441d
dk949 <56653556+dk949@users.noreply.github.com>
2025-09-22 03:37:04 +0100

    plugin(neorg): tried to fix indentation

--- lua/config/plugins/neorg.lua
+++ lua/config/plugins/neorg.lua
@@ -8,10 +8,17 @@ return {
         },
         ["core.integrations.image"] = {},
         ["core.latex.renderer"] = {},
+        ["core.esupports.indent"] = {
+            config = {
+                format_on_enter = false,
+                format_on_escape = false,
+                indents = { ranged_verbatim_tag_content = 0 },
+            }
+        },
         ["core.export"] = {},
         ["core.dirman"] = {
             config = {
-                workspaces = { notes = "~/Uni/notes/", },
+                workspaces = { notes = "~/Uni/notes/", womp = "~/Uni/code/xdsl/womp/notes" },
                 default_workspace = "notes",
                 open_last_workspace = false, -- maybe?
             },
