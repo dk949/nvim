8d80dabf244e2ffc8bc8dc6b5e51bcd7ac9780bb
dk949 <56653556+dk949@users.noreply.github.com>
2025-09-30 10:21:18 +0100

    plugin(vimspector): don't hardcode vimspacetor base dir

--- lua/config/plugins/vimspector.lua
+++ lua/config/plugins/vimspector.lua
@@ -1,5 +1,5 @@
 return function()
-    vim.g.vimspector_base_dir = '/home/davidk/.local/share/nvim/lazy/vimspector'
+    vim.g.vimspector_base_dir = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy/vimspector")
     vim.g.vimspector_enable_mappings = 'HUMAN'
     vim.g.vimspector_enable_winbar = true
 end
