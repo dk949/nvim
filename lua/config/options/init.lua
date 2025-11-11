654efdbb358b8c5d85db11dae1c582d9650619ba
dk949 <56653556+dk949@users.noreply.github.com>
2025-11-11 22:19:47 +0000

    config: set global border to rounded

--- lua/config/options/init.lua
+++ lua/config/options/init.lua
@@ -11,6 +11,7 @@ vim.opt.diffopt:append("vertical")
 -- When reading in file with :r, don't set read file as alt
 vim.opt.cpoptions:remove('a')
 vim.opt.exrc = true
+vim.opt.winborder = "rounded"
 
 require("config.options.stdin")
 require("config.options.line_numbers")
