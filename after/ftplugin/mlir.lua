b33ebbd4fbbd576e2a3347ef315497f0dccb0029
dk949 <56653556+dk949@users.noreply.github.com>
2025-11-11 22:10:43 +0000

    lang(mlir): no sign or colorcolumn

--- after/ftplugin/mlir.lua
+++ after/ftplugin/mlir.lua
@@ -1,6 +1,8 @@
 require "utils".ftplugin(
     require "config.common".prog:with {
         makeprg = "lit -vv %",
+        signcolumn = "no",
+        colorcolumn = vim.NIL,
         ["@indentBlankline"] = false,
     }
 )
