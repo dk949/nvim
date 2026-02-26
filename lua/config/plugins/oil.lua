5d238ad2dea3014663d968b20fdacb466e85e6cb
dk949 <56653556+dk949@users.noreply.github.com>
2026-02-26 15:41:47 +0000

    plugin(oil): run aucmd when creating files with oil

--- lua/config/plugins/oil.lua
+++ lua/config/plugins/oil.lua
@@ -1,3 +1,4 @@
+---@module "oil"
 local keymap = require("config.keymap")
 local maps = keymap.oil:transform(function(_, m)
     local mode
@@ -8,6 +9,33 @@ local maps = keymap.oil:transform(function(_, m)
         return m.lhs, m.rhs
     end
 end)
+
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = "OilActionsPost",
+    ---comment
+    ---@param args {data: {actions: oil.Action[], err: string?}}
+    callback = function(args)
+        if args.data.err then return end
+        for _, action in ipairs(args.data.actions) do
+            if action.type ~= "create"
+                or action.entry_type ~= "file"
+                or not vim.startswith(action.url, "oil:///") then
+                goto continue
+            end
+            ---@type string
+            local path = action.url:sub(7)
+            local buf = vim.api.nvim_create_buf(false, false)
+            vim.api.nvim_buf_set_name(buf, vim.fs.relpath(vim.fn.getcwd(), path) or path)
+            vim.api.nvim_buf_call(buf, function()
+                vim.cmd [[doautocmd BufNewFile]]
+                vim.cmd [[write!]]
+            end)
+            ::continue::
+        end
+    end,
+})
+
 return {
     win_options = {
         signcolumn = "yes:2",
