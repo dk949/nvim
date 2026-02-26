6a3ccb65021670d90af82a2439a2147f20211e42
dk949 <56653556+dk949@users.noreply.github.com>
2026-02-26 15:42:33 +0000

    lang(cmake): disable cmake language server

--- after/ftplugin/cmake.lua
+++ after/ftplugin/cmake.lua
@@ -1,12 +1,13 @@
 require "utils".ftplugin(
     require "config.common".prog:with {
         formatprg = require("config.common.formatting").cmakePrg,
+        ["@treesitter"] = { "highlight" },
     },
     function()
         require "utils.lsp".enableLspTools {
             config = "cmake",
             mason = {
-                "cmake-language-server",
+                -- "cmake-language-server",
                 "cmakelang",
             },
         }
