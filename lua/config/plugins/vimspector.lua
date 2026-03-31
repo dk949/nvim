8cc94f52c714ac518e0e68645caf13bb89c5739b
dk949 <56653556+dk949@users.noreply.github.com>
2026-03-31 01:50:18 +0100

    plugin(vimspector): default python configuration

--- lua/config/plugins/vimspector.lua
+++ lua/config/plugins/vimspector.lua
@@ -2,4 +2,26 @@ return function()
     vim.g.vimspector_base_dir = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy/vimspector")
     vim.g.vimspector_enable_mappings = 'HUMAN'
     vim.g.vimspector_enable_winbar = true
+    vim.g.vimspector_configurations = {
+        python = {
+            adapter = 'debugpy',
+            configuration = {
+                type = 'python',
+                request = 'launch',
+                python = "${VIRTUAL_ENV}/bin/python",
+                cwd = "${fileDirname}",
+                justMyCode = false,
+                args = {"*${Args}"},
+                program = "${file}",
+            },
+            breakpoints = {
+                exception = {
+                    raised = 'N',
+                    caught = 'N',
+                    uncaught = 'Y',
+                    userUnhandled = 'N',
+                },
+            },
+        },
+    }
 end
