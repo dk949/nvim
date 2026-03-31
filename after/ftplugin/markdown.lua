7c475a1bd56915ffd9a591ea06a43170bf590cbe
dk949 <56653556+dk949@users.noreply.github.com>
2026-03-31 01:47:52 +0100

    lang(markdown): use notes_comment and do not use logical lines

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,4 +1,8 @@
 require "utils".ftplugin(
-    require "config.common".text:with({ ["@treesitter"] = { "highlight" } }),
+    require "config.common".text:with({
+        ["@treesitter"] = { "highlight" },
+        ["@notesComment"] = true,
+        ["@logicalLines"] = vim.NIL,
+    }),
     function() require "utils.lsp".enableLspTools() end
 )
