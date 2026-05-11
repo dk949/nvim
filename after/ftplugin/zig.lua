b1a0cef95fffb9aa7858d2a676fba4adacc5168a
dk949 <56653556+dk949@users.noreply.github.com>
2026-05-11 17:40:11 +0100

    lang(zig): fix zig env output parsing

--- after/ftplugin/zig.lua
+++ after/ftplugin/zig.lua
@@ -84,16 +84,10 @@ require "utils".ftplugin(
                 log.sched.warn("Couldn't load zig environment: ", out.stderr)
                 return
             end
-            ---@type boolean, table
-            local succ, res = pcall(vim.json.decode, out.stdout)
-            if not succ then
-                log.sched.warnf("Could not decode zig env: '%s'", out.stdout)
-                return
-            end
             ---@type string?
-            local std_dir = res.std_dir
+            local std_dir = out.stdout:match([[%s*%.std_dir%s*=%s*"(.-)"]])
             if not std_dir then
-                log.warn("Failed to read std_dir from ", res)
+                log.warn("Failed to read std_dir from ", out.stdout)
                 return
             end
             vim.g.zig_std_dir = std_dir
