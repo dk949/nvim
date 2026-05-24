af72b300e0923fc921c978550220e6bd4623aaf4
dk949 <56653556+dk949@users.noreply.github.com>
2026-05-24 20:52:05 +0100

    lang(rust): disable spellcheck

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,6 +1,7 @@
 require "utils".ftplugin(
     require "config.common".prog:with {
         formatprg = require("config.common.formatting").rustPrg,
+        spell = false,
     },
     function()
         require "utils.lsp".enableLspTools({ config = "rust_analyzer", mason = "rust-analyzer" })
